91f963446d8bf92a0b1f45ee07dece1f00dcc77b
Sophia09 <lisai121121@gmail.com>
2014-09-17 11:13:00 +0800

    first commit

---
+++ Singleton/Singleton.h
@@ -0,0 +1,13 @@
+//
+//  Singleton.h
+//  Singleton
+//
+//  Created by thinkit  on 9/17/14.
+//  Copyright (c) 2014 thinkit . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Singleton : NSObject
+
+@end
---
+++ Singleton/Singleton.m
@@ -0,0 +1,13 @@
+//
+//  Singleton.m
+//  Singleton
+//
+//  Created by thinkit  on 9/17/14.
+//  Copyright (c) 2014 thinkit . All rights reserved.
+//
+
+#import "Singleton.h"
+
+@implementation Singleton
+
+@end
